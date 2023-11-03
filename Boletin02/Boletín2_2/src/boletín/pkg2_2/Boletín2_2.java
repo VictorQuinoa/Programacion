c41833af8d3657b02f240106bbbb7641d0ebd977
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2023-11-03 13:34:18 +0100

    02_2

---
+++ Boletin02/Boletín2_2/src/boletín/pkg2_2/Boletín2_2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletín.pkg2_2;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletín2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float Lado= 3, Área;
+        Área= Lado*Lado;
+        System.out.println ("Área ="+Área);
+    }
+    
+}

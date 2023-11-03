55b96f4fcb4ffe3b9930744821aa6af603b3b5a6
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2023-11-03 13:54:00 +0100

    09_4

---
+++ Boletin09/Boletin09_4/src/main/java/com/mycompany/boletin09_4/Boletin09_4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boletin09_4;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin09_4 {
+
+    public static void main(String[] args) {
+        Tablas obx = new Tablas();
+        obx.verTabla();
+    }
+}
---
+++ Boletin09/Boletin09_4/src/main/java/com/mycompany/boletin09_4/Tablas.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.boletin09_4;
+
+
+
+/**
+ *
+ * @author dam1
+ */
+public class Tablas {
+    public void verTabla(){
+       
+        int num=(int) LerDato.lerFloatPositivo("Introduce un número:");
+        
+      do{
+           System.out.println("Tabla de multiplicar:");
+           System.out.println("1:"+ num *1);
+           System.out.println("1:"+ num *2);
+           System.out.println("1:"+ num *3);
+           System.out.println("1:"+ num *4);
+           System.out.println("1:"+ num *5);
+           System.out.println("1:"+ num *6);
+           System.out.println("1:"+ num *7);
+           System.out.println("1:"+ num *8);
+           System.out.println("1:"+ num *9);
+           System.out.println("1:"+ num *10);
+       }while(num != 0);
+     
+     
+       
+        
+            
+                
+    }
+    
+}

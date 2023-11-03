f5df48b3c310cfd510e1210d729bd76c7b636da3
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2023-11-03 13:52:43 +0100

    09_2

---
+++ Boletin09/Boletin09_2/src/main/java/com/mycompany/boletin09_2/Boletin09_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boletin09_2;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin09_2 {
+
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ Boletin09/Boletin09_2/src/main/java/com/mycompany/boletin09_2/sumarProducto.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.boletin09_2;
+
+public class sumarProducto {
+    public void sumarProducto(){
+        int suma= 0;
+        double producto = 1;
+        int numero = 10;
+        
+        while (numero <= 90) {
+            suma +=numero;
+            producto*=+producto;
+            número
+                    
+        }
+    }
+    
+}

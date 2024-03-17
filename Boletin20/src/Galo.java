2d702e7628d5d9081353941e055bf92e11ea321e
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2024-03-17 13:03:29 +0100

    Boletin20

---
+++ Boletin20/src/Galo.java
@@ -0,0 +1,10 @@
+
+
+public class Galo implements IPodeCantar{
+
+    @Override
+    public void cantar(){
+        System.out.println("Kikirikí");
+    }
+
+}
---
+++ Boletin20/src/IPodeCantar.java
@@ -0,0 +1,4 @@
+public interface IPodeCantar {
+
+    public void cantar();
+}

0ea29a3ebe10802789c3212b18de476a822cff38
VictorQuinoa <145323734+VictorQuinoa@users.noreply.github.com>
2024-03-18 18:53:54 +0100

    Add files via upload

---
+++ Boletin 20/Canario.java
@@ -0,0 +1,8 @@
+public class Canario implements IPodeCantar {
+
+    @Override
+    public void cantar(){
+        System.out.println("Pio pio");
+    }
+
+}
---
+++ Boletin 20/Persoa.java
@@ -0,0 +1,8 @@
+
+public class Persoa implements IPodeCantar {
+
+    @Override
+    public void cantar(){
+        System.out.println("DO RE MI FA SOL LA SI");
+    }
+}
